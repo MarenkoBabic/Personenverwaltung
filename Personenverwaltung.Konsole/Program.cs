d5bfecb8e5de24be52cd75f886c2b5e8afa3ee21
Marenko Babic <marinko_@hotmail.com>
2018-05-23 15:01:10 +0200

    Projektdateien hinzufügen.

---
+++ Personenverwaltung.Konsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personenverwaltung.Konsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
